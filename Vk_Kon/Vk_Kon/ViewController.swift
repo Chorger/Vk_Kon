fde01341d98a20b7548c5ea6e8e473eb34a8c131
Timofey <tim361234567890@gmail.com>
2019-08-20 10:51:52 +0300

    First commit and upload

---
+++ Vk_Kon/Vk_Kon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vk_Kon
+//
+//  Created by Timofey Koloshenko on 20/08/2019.
+//  Copyright © 2019 Timofey Koloshenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
