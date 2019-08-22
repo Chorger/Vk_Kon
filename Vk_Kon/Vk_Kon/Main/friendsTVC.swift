71a5d49d650360fa2689c67c0c9e262c65d91f19
Timofey <tim361234567890@gmail.com>
2019-08-22 10:36:35 +0300

    Переделал заново все контроллеры в поисках решения проблемы ("схлопывались" ячейки и не было содержимого), чтобы понять, что нужно было задать высоту constraint'ом. Написал код для создания и наполнения ячеек. Создал Collection View Controller и привязал класс ему. Добавил картинки.

---
+++ Vk_Kon/Vk_Kon/Main/friendsTVC.swift
@@ -0,0 +1,80 @@
+//
+//  friendsTVC.swift
+//  Vk_Kon
+//
+//  Created by Timofey Koloshenko on 22/08/2019.
+//  Copyright © 2019 Timofey Koloshenko. All rights reserved.
+//
+
+import UIKit
+
+class friendsTVC: UITableViewController {
+    
+    var friendsList : [User] = []
+    var imagesNames : [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        friendsList.append(User(name: "Аня Мех", image: "message_annImage"))
+        friendsList.append(User(name: "Ася Гагарина", image: "asyagagarinamskImage"))
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friendsList.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath)
+
+        cell.imageView!.image = UIImage(named: friendsList[indexPath.row].image)
+        cell.textLabel!.text = friendsList[indexPath.row].name
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
